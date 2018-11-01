fc38b8c627a0784a527becd723c482d26d24d4ac
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-11-01 15:51:08 +0900

    プレイヤーを動けるようにした。木の障害物も置いてみた(雰囲気のため)　あとアセット保管フォルダの名前を「DL_Asset」に変更

---
+++ GFF2019_Team4 Conveni/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveForce = 5, SneakMultiplier = 1;
+    Rigidbody2D myBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myBody = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+        bool isSneaking = CrossPlatformInputManager.GetButton("Sneak");
+        //Debug.Log(isSneaking ? SneakMultiplier : 1);
+
+        myBody.AddForce(moveVec * (isSneaking ? SneakMultiplier : 1));
+    }
+}
