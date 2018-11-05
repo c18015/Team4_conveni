ba28ed0da19fb86ac4a4f7ffdfe4759fa18d9b9d
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-11-05 16:45:30 +0900

    蜘蛛さんの感知範囲にプレイヤーが入るとプレイヤーを追いかけるようになたよ～^^

--- GFF2019_Team4 Conveni/Assets/Scripts/Sensing.cs
+++ GFF2019_Team4 Conveni/Assets/Scripts/Sensing.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class Sensing : MonoBehaviour
 {
+    public GameObject player;
+    public float MoveSpeed = 0;
 
-    // Use this for initialization
+
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
 
+       
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
 
     }
 
-    void OnTriggerStay2D(Collider2D other)
+
+    void OnTriggerStay2D(Collider2D other)　//プレイヤーがコライダー(感知範囲)に入ったかを判断
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("感知圏内にプレイヤーが入りました。");
 
-            //Destroy(this.gameObject);
+            PlayerLook();　
+
         }
+    }
+
+    void PlayerLook()  //蜘蛛がプレイヤーの方を向いて動く
+    {
+
+        var vec = (player.transform.position - this.transform.position).normalized;
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.up, vec);
+
+        transform.Translate(0, 1 * MoveSpeed * Time.deltaTime, 0);
+
     }
 }
