8f70c89b15edbb0fe505296199aec963f52d53d8
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-11-15 15:37:29 +0900

    発電メニューを開けるようにした。いろいろ問題はあるかもしれないがとりまこれで。

---
+++ GFF2019_Team4 Conveni/Assets/Scripts/LightButtonCon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightButtonCon : MonoBehaviour {
+
+    public GameObject hatudenGUI;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+    public void LightGuiON()
+    {
+        hatudenGUI.SetActive(true);
+
+    }
+
+    public void LightGuiOFF()
+    {
+        hatudenGUI.SetActive(false);
+
+    }
+}
