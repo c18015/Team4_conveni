d0cc340aaf6acc879a4a16748b23f51f0f0a96ae
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-11-15 14:13:13 +0900

    発電システム(仮)その１　　必要そうなGUIなどを画面に追加した

---
+++ GFF2019_Team4 Conveni/Assets/Scripts/LightGauge_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightGauge_Controller : MonoBehaviour
+{
+    public float countTime;
+    Slider Light_meter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Light_meter = GameObject.Find("Light Gauge").GetComponent<Slider>();
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime -= Time.deltaTime;
+
+
+        
+        if (countTime > Light_meter.maxValue)
+        {
+            countTime = Light_meter.maxValue;
+
+        }
+
+        Light_meter.value = countTime; 
+
+    }
+}
+
+
+
+
+
+
