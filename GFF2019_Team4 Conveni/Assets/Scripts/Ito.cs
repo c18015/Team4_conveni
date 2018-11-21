71b0e4ec4ddbbaca2e7f505ca2e964aedd2788b1
SaeMatayoshi <c18016@std.it-college.ac.jp>
2018-11-21 15:39:48 +0900

    糸を引きちぎるスクリプトを試している途中

---
+++ GFF2019_Team4 Conveni/Assets/Scripts/Ito.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ito : MonoBehaviour {
+
+    bool Isarea = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Isarea = true;
+        Debug.Log("入ってまーす!");
+        Debug.Log(Isarea);
+
+        TouchInput.Started += info => 
+        {
+            Debug.Log("いと切れた");
+        };
+    }
+}
