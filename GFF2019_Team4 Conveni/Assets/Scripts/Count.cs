72b559d86b241e7d57ff20e8e9b36d4775b25e9c
SaeMatayoshi <c18016@std.it-college.ac.jp>
2018-12-03 14:01:58 +0900

    【済】3人目で蜘蛛出現

---
+++ GFF2019_Team4 Conveni/Assets/Scripts/Count.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Count : MonoBehaviour {
+
+    GameObject[] tagObjects;
+    public bool Iscount = false;
+
+    //float timer = 0.0f;
+    //float interval = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*timer += Time.deltaTime;
+
+        if(timer > interval)
+        {
+            Check("Friend");
+
+            timer = 0;
+        }*/
+
+        Check("Friend");
+	}
+
+    void Check(string tagname)
+    {
+        tagObjects = GameObject.FindGameObjectsWithTag(tagname);
+
+        Debug.Log(tagObjects.Length);
+        if(tagObjects.Length == 3)
+        {
+            Iscount = true;
+            //Debug.Log(Iscount);
+        }
+
+        if(tagObjects.Length == 0)
+        {
+            Debug.Log(tagname + "タグがついたオブジェクトはありません");
+        }
+    }
+}
--- GFF2019_Team4 Conveni/Assets/Scripts/Ito.cs
+++ GFF2019_Team4 Conveni/Assets/Scripts/Ito.cs
@@ -3,37 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(TouchInput))]
 public class Ito : MonoBehaviour
 {
-
-    bool Isarea = false;
+    bool Isarea = false;   
     public TouchInput touchInput;
+    public Count count;
+
+    public GameObject kumo;
+
+    RaycastHit2D hitObject;
+
+    bool Iscount;
+    
+
+    //private int count;
 
     // Use this for initialization
     void Start()
     {
-
+        //Iscount = count.Iscount;
     }
 
     // Update is called once per frame
     void Update()
     {
-          
+        Debug.Log(count.Iscount);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (count.Iscount == true)
+        {
+            Instantiate(
+                kumo,
+                gameObject.transform.position,
+                Quaternion.identity
+                );
+
+            Debug.Log("蜘蛛出現！");
+            
+        }
         //Isarea = true;
 
-        Debug.Log("入ったよ－！");
+        //Debug.Log("入ったよ－！");
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         Isarea = true;
-        Debug.Log("入ってまーす!");
-        Debug.Log(Isarea);
+        //Debug.Log("入ってまーす!");
+        //Debug.Log(Isarea);
 
         if (collision.gameObject.tag == "Player")
         {
@@ -45,27 +64,33 @@ public class Ito : MonoBehaviour
                     tapPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     Collider2D collider2D = Physics2D.OverlapPoint(tapPosition);
 
-                    Debug.Log(tapPosition);
-
                     if (collider2D)
                     {
                         RaycastHit2D hitObject = Physics2D.Raycast(tapPosition, Vector2.up);
                         if (hitObject.collider.gameObject.tag == "Friend")
                         {
+                            
+
                             hitObject.collider.gameObject.SetActive(false);
-                            Debug.Log("いと切れた！");
+                                                        
+                            //Debug.Log("いと切れた！");    
+                            if (hitObject.collider.gameObject == false)
+                            {
+                                
+                            }
                         }
+                        
                     }
-                    //this.gameObject.SetActive(false);
+                    
                     Isarea = false;
-                }
+                }                
             };
-        }
+        }        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("出たよー！");
+        //Debug.Log("出たよー！");
         Isarea = false;
     }
 }
