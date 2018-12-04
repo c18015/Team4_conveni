7dcc094c1352f991c38932f7b117a86776100752
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-12-04 11:54:23 +0900

    タイトルシーンが仮段階だが完成、メインシーンに飛べるようにもした。

---
+++ GFF2019_Team4 Conveni/Assets/Scenes/TitleSceneScript/GoMain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickStartButton()
+    {
+        Application.LoadLevel("Main");
+    }
+}
---
+++ GFF2019_Team4 Conveni/Assets/Scenes/TitleSceneScript/TitleGimmick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleGimmick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var pos = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var rotation = Quaternion.LookRotation(Vector3.forward, Input.mousePosition - pos);
+        transform.localRotation = rotation;
+    }
+
+}
