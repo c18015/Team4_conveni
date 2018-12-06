4e8ec94b1aaaa8ad7bd5df996f8159c4b1bce728
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-12-06 11:27:58 +0900

    タイトルにあるスタート、操作説明のUIをデザイナーが書いてくれたものに置き換えた。 アプリを終了するスクリプトを追加、ビルド出来てないから動作を確認できてないけどできてると思う

---
+++ GFF2019_Team4 Conveni/Assets/Scenes/TitleSceneScript/GM_EscapeProgram.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM_EscapeProgram : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* if (Input.GetKey("escape"))
+         {
+             Debug.Log("おわわあわわｗｗｗｗ");
+             Application.Quit();
+         }*/
+
+    }
+
+    public void GameEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+		Application.Quit();
+#endif
+    }
+}
